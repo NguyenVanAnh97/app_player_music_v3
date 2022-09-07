6f7f104ed43486432130fa26614aa58facf2ecd1
vace <nguyenvananh270897@gmail.com>
2022-09-07 23:23:10 +0700

    add_music

---
+++ app.js
@@ -0,0 +1,88 @@
+const $ = document.querySelector.bind(document);
+const $$ =  document.querySelectorAll.bind(document);
+
+songs: [
+    {
+        name: "Tòng phu",
+        singer: "KEYO",
+        path: "./assets/music/KEYO TONG PHU.mp3",
+        image:"./assets/img/Tòng phu.jpg"
+    },
+    {
+      name: "Bước Qua Nhau",
+      singer: "Vũ",
+      path: "./assets/music/Buoc Qua Nhau - Vu.mp3",
+      image: "./assets/img/Bước Qua Nhau.jpg"
+    },
+    {
+      name: "Chạy Khỏi Thế Giới Này",
+      singer: "DaLab x Phương Ly",
+      path: "./assets/music/Chay Khoi The Gioi Nay - Da LAB_ Phuong.mp3",
+      image:
+        "./assets/img/Chạy Khỏi Thế Giới Này.jpg"
+    },
+    {
+      name: "Dang Dở",
+      singer: "Nal",
+      path:
+        "./assets/music/Dang Do - Nal.mp3",
+      image: "./assets/img/Dang Dở.jpg"
+    },
+    {
+      name: "Đập vỡ cây đàn",
+      singer: "Quang Lê",
+      path: "./assets/music/Dap Vo Cay Dan - Quang Le.mp3",
+      image:
+        "./assets/img/Đập Vỡ Cây Đàn.jpg"
+    },
+    {
+      name: "Hạ Còn Vương Nắng",
+      singer: "Raftaar",
+      path: "./assets/music/Ha Con Vuong Nang - DatKaa.mp3",
+      image:
+        "./assets/img/Hạ Còn Vương Nắng.jpg"
+    },
+    {
+      name: "Hayya Hayya Better Together",
+      singer: "Trinidad",
+      path:
+        "./assets/music/Hayya Hayya Better Together_ - Trinidad.mp3",
+      image:
+        "./assets/img/Hayya Hayya.jpg"
+    },
+    {
+        name: "Khi cô đơn em nhớ đến ai",
+        singer: "Grey D x Suni",
+        path: "./assets/music/Khi Co Don Em Nho Den Ai - Grey D_ Suni.mp3",
+        image:
+          "./assets/img/Khi Cô Đơn Em Nhớ Đến Ai.jpg"
+      },
+      {
+        name: "Muộn Rồi Mà Sao Còn",
+        singer: "Sơn Tùng M-TP",
+        path: "./assets/music/Muon Roi Ma Sao Con - Son Tung M-TP.mp3",
+        image:
+          "./assets/img/Muộn Rồi Mà Sao Còn.jpg"
+      },
+      {
+        name: "Nàng thơ",
+        singer: "Hoàng Dũng",
+        path: "./assets/music/Nang Tho - Hoang Dung.mp3",
+        image:
+          "./assets/img/Nàng Thơ.jpg"
+      },
+      {
+        name: "Nặng Tình Hay Nhẹ Lòng",
+        singer: "Tống Gia Vỹ",
+        path: "./assets/music/Nang Tinh Hay Nhe Long WRC Remix_ - ZIN.mp3",
+        image:
+          "./assets/img/Nặng Tình Hay Nhẹ Lòng.jpg"
+      },
+      {
+        name: "Sài Gòn Đau Lòng Quá",
+        singer: "HỨA KIM TUYỀN x HOÀNG DUYÊN",
+        path: "./assets/music/Sai Gon Dau Long Qua - Hua Kim Tuyen_ Ho.mp3",
+        image:
+          "./assets/img/Sài Gòn Đau Lòng Quá.jpg"
+      },
+  ]
